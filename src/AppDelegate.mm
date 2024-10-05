b12e84611092945c262f910a9c18a539ce227ac8
matt petters <mcpetters@gmail.com>
2024-10-04 19:35:16 -0700

    init with files

---
+++ src/AppDelegate.mm
@@ -0,0 +1,13 @@
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
